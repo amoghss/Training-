66ef5a8454369f4d267ed1b43f07a352b6c95d34
amoghss <57131377+amoghss@users.noreply.github.com>
2021-09-23 18:13:14 +0530

    Delete Introduction-to-java-2/Answer1 directory

--- Introduction-to-java-2/Answer1/src/com/answer1/service/TransactionService.java
+++
@@ -1,8 +0,0 @@
-package com.answer1.service;
-
-public interface TransactionService {
-	
-	int addTransaction();		// adds a new transaction
-	int endTransaction();		// adds return date to transaction
-
-}
